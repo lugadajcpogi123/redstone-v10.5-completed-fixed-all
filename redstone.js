05eb406bd1318f70090282945c8cc8eb9a6c0ef0
lugadajcpogi123 <jckugadapogi@gmail.com>
2015-03-02 22:11:57 +0800

    Update and rename .gitignore to redstone.js

---
+++ redstone.js
@@ -0,0 +1,25 @@
+var redstone = 1; (i = 0;)
+
+function redstone = 0; (i = 1 < = 3;)
+
+var redstone mod = true
+redstone dust = true (can stick on wall)
+repeater = true
+redstone torch = true
+redstone lamp = true
+sticky piston = true
+piston = true
+stone button = true
+wooden button = true
+stone pleasure plate = true
+wooden pleasure plate = true
+lever = true
+iron door = true
+detector rail = true
+activator rail = true
+
+                       //code\\
+if redstone dust on with lever or redstone torch tnt will blow up
+
+rZ creeper rX creeper rY creeper < = texture pigmen.png
+rZ creeper rX creeper rY creeper (drop 30 gold ingots)
